714bfbee78d9ba6dc42249b80c85afa051e6f3e7
leesangun <leesangun@Leeui-MacBook-Pro-2.local>
2020-06-18 12:57:10 +0900

    SuperSocket

---
+++ Client/ClientUnity/Assets/1_Script/Tcp/ClientTCP.cs
@@ -0,0 +1,137 @@
+﻿
+using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+public class ClientTCP  {
+
+	private Socket _socket;
+ 
+        public readonly string SERVER_IP = "127.0.0.1";
+        public readonly int SERVER_PORT = 4000;
+ 
+        private byte[] _byteReceive = new byte[2000];    // Receive data by this array to save.
+ 
+        public delegate void Ack(string data);
+        private Ack _ack;
+ 
+        private static ClientTCP _clientTCP;
+        public static ClientTCP getInstance(Ack ack)
+        {
+            if (_clientTCP == null)
+            {
+                _clientTCP = new ClientTCP();
+                _clientTCP.start(ack);
+            }
+            return _clientTCP;
+        }
+ 
+ 
+        private void start(Ack ack)
+        {
+ 
+            _ack = ack;
+ 
+            //=======================================================
+            // Socket create.
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendTimeout, 10000);
+            _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 10000);
+ 
+            //=======================================================
+            // Socket connect.
+            try
+            {
+                IPAddress ipAddr = System.Net.IPAddress.Parse(SERVER_IP);
+                IPEndPoint ipEndPoint = new System.Net.IPEndPoint(ipAddr, SERVER_PORT);
+                _socket.Connect(ipEndPoint);
+            }
+            catch (SocketException SCE)
+            {
+                Console.WriteLine("Socket connect error! : " + SCE.ToString());
+                return;
+            }
+ 
+            this.beginReceive();
+ 
+        }
+ 
+        public void end()
+        {
+            _socket.Close();
+            _socket = null;
+        }
+ 
+        public void write(string message)
+        {
+            try
+            {
+                //int SenddataLength = Encoding.Default.GetByteCount(message);
+                //byte[] bytes = Encoding.Default.GetBytes(message);
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                _socket.Send(bytes, bytes.Length, 0);
+            }
+            catch (SocketException err)
+            {
+                Console.WriteLine("Socket send or receive error! : " + err.ToString());
+            }
+        }
+ 
+        public void write(byte[] bytes)
+        {
+            try
+            {
+                _socket.Send(bytes, bytes.Length, 0);
+            }
+            catch (SocketException err)
+            {
+                Console.WriteLine("Socket send or receive error! : " + err.ToString());
+            }
+        }
+ 
+        /*
+        public void read()
+        {
+            try
+            {
+                _socket.Receive(Receivebyte);
+                ReceiveString = Encoding.Default.GetString(Receivebyte);
+                Debug.Log("Receive Data : " + ReceiveString + "(" + Encoding.Default.GetByteCount(ReceiveString.ToString()) + ")");
+            }
+            catch (SocketException err)
+            {
+                Debug.Log("Socket send or receive error! : " + err.ToString());
+            }
+        }
+        */
+ 
+        private int _recieved;
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            _recieved = _socket.EndReceive(AR);
+ 
+            if (_recieved <= 0) return;
+ 
+            byte[] recData = new byte[_recieved];
+            Buffer.BlockCopy(_byteReceive, 0, recData, 0, _recieved);
+ 
+            //this._interfaceClient.read(recData);
+            //Debug.Log(Encoding.Default.GetString(recData));
+            _ack(Encoding.UTF8.GetString(recData));
+            _recieved = 0;
+ 
+            this.beginReceive();
+        }
+ 
+        private void beginReceive()
+        {
+            _socket.BeginReceive(_byteReceive, 0, _byteReceive.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+        }
+ 
+        public void update()
+        {
+ 
+ 
+        }
+}
---
+++ Client/ClientUnity/Assets/1_Script/Tcp/SceneTCP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTCP : MonoBehaviour {
+	private ClientTCP _clientTCP;
+	
+	void Start () {
+		_clientTCP = ClientTCP.getInstance((string data)=>{
+			print(data);
+		});
+	}
+	
+	public void ClickSend(){
+		_clientTCP.write("aaa");
+	}
+}
